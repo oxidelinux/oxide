e9251657f2efe0c687c91e6587b9817b91d147d1
arshavir mirzakhani <76659587+arshavirmirzakhani@users.noreply.github.com>
2024-08-06 14:31:31 +0330

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,17 +3,11 @@
 # Set the name of the distro
 DISTRO_NAME="oxide"
 # Set the Debian version
-DEBIAN_VERSION="bookworm"
+DEBIAN_VERSION="bullseye"
 # Set image version
 VERSION="1.0.0-alpha"
 # Set image publisher
 PUBLISHER="arshavir mirzakhani - github.com/arshavirmirzakhani"
-# Set boot logo
-BOOT_LOGO="assets/logo.png"
-# Set KDE logo
-KDE_LOGO="assets/logo.png"
-# Set background image
-BACKGROUND_IMAGES="assets/wallpapers"
 
 # Set the architecture array
 declare -a archs=("i386" "amd64")
@@ -35,14 +29,16 @@ do
     --iso-volume $DISTRO_NAME-$VERSION \
     --system normal
   
-    # Customize live build config
+  # Customize live build config
   cat ../packages.txt >> config/package-lists/desktop.list.chroot
 
+  # Copy all chroot scripts
   cp -a ../chroot/* config/hooks/normal/
   
   # Run lb build to create the live image
   lb build
 
+  # Rename resulting iso file
   mv live-image-$arch.hybrid.iso $DISTRO_NAME-$VERSION-$arch.iso
 
   cd ..
